2aa886f287b16081f4ede1c71cf457fabed1bd9a
hawk1000525 <josephaham@hotmail.com>
2017-05-05 10:05:58 -0500

    Delete zeros (1).py

--- zeros (1).py
+++
@@ -1,7 +0,0 @@
-###########################################################################################
-# Name: 
-# Date: 
-# Description: This program calculates the number of zeros written from one to one million.
-###########################################################################################
-
-# write your code below (and remove this comment...)
