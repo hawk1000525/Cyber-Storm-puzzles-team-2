80b28ab3ffa0210fd80da9c46d3ebea70876cb2c
hawk1000525 <josephaham@hotmail.com>
2017-05-05 10:03:34 -0500

    Add files via upload

---
+++ zeros (1).py
@@ -0,0 +1,7 @@
+###########################################################################################
+# Name: 
+# Date: 
+# Description: This program calculates the number of zeros written from one to one million.
+###########################################################################################
+
+# write your code below (and remove this comment...)
