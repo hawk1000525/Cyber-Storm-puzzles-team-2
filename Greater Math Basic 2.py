c6c6049daa365d464d38fda8fae24821f93abf41
JohnWolz <johnwolz211@gmail.com>
2017-05-11 18:59:24 -0500

    Update Greater Math Basic 2.py
    This is the portion of the puzzle for the team on the right side of the main pi

--- Greater Math Basic 2.py
+++ Greater Math Basic 2.py
@@ -53,4 +53,4 @@ while state == True:
 
     if points == 10:
         state = False
-        print "You have won! Here is the next piece of password ''"
+        print "You have won! Here is the next piece of password 'u7v'"
